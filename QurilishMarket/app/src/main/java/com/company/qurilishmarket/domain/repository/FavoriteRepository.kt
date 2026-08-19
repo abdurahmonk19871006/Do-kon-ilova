834c8ee5096903034eb5866b2fbd6a0619cf24e8
abdurahmonk19871006 <abdurahmonk1987.1006@gmail.com>
2026-08-19 22:11:46 +0500

    Delete QurilishMarket directory

--- QurilishMarket/app/src/main/java/com/company/qurilishmarket/domain/repository/FavoriteRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.company.qurilishmarket.domain.repository
-
-import com.company.qurilishmarket.domain.model.Product
-
-interface FavoriteRepository {
-    suspend fun getFavoriteProducts(): Result<List<Product>>
-    suspend fun isFavorite(productId: String): Result<Boolean>
-    suspend fun toggleFavorite(productId: String, isFavorite: Boolean): Result<Unit>
-}
