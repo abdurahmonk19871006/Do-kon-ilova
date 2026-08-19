5ecad140d40a06466e09e8c5e9be7b80e76dba60
abdurahmonk19871006 <abdurahmonk1987.1006@gmail.com>
2026-08-19 22:12:34 +0500

    Delete src/main directory

--- src/main/java/com/company/qurilishmarket/domain/repository/AddressRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.company.qurilishmarket.domain.repository
-
-import com.company.qurilishmarket.domain.model.Address
-
-interface AddressRepository {
-    suspend fun createAddress(title: String, fullAddress: String): Result<String>
-    suspend fun getMyAddresses(): Result<List<Address>>
-    suspend fun deleteAddress(addressId: String): Result<Unit>
-}
